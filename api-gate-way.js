f1a8b09e0a7c536717e60f5529c94e7167f0ef95
tuandao <tuandao251196@gmail.com>
2018-10-22 14:44:41 +0700

    version clustering kms

--- api-gate-way.js
+++ api-gate-way.js
@@ -2,14 +2,16 @@ const jayson = require('jayson');
 const rpcBuilder = require('kurento-jsonrpc');
 
 
-const socket = require('socket.io-client')('http://localhost:3000');
+// const socket = require('socket.io-client')('http://localhost:3000');
+const socket = require('socket.io-client')('https://tuan-dao.herokuapp.com');
 
 
 var JsonRpcClient = rpcBuilder.clients.JsonRpcClient;
 
 
-// const ws_uri = "ws://localhost:8888/kurento";
-const ws_uri = "ws://35.207.205.137:8888/kurento";
+// const ws_uri = ["ws://localhost:8888/kurento"];
+const ws_uri = ["ws://34.207.205.137:8888/kurento",
+"ws://35.231.76.154:8888/kurento"];
 
 
 function connectCallback(){
@@ -39,28 +41,52 @@ function onEvent(_message) {
     // socket.emit('candidate',JSON.stringify(_message));
 }
 
-let configuration = {
-    sendCloseMessage : false,
-    ws : {
-      uri : ws_uri,
-      useSockJS: false,
-      onconnected : connectCallback,
-      ondisconnect : disconnectCallback,
-      onreconnecting : disconnectCallback,
-      onreconnected : connectCallback,
-      onerror : errorCallback
-    },
-    rpc : {
-      requestTimeout : 15000,
-      onEvent : onEvent
-    }
-};
+// var configuration = {
+//     sendCloseMessage : false,
+//     ws : {
+//       uri : ws_uri[0],
+//       useSockJS: false,
+//       onconnected : connectCallback,
+//       ondisconnect : disconnectCallback,
+//       onreconnecting : disconnectCallback,
+//       onreconnected : connectCallback,
+//       onerror : errorCallback
+//     },
+//     rpc : {
+//       requestTimeout : 15000,
+//       onEvent : onEvent
+//     }
+// };
 
-let jsonRpcClient = new JsonRpcClient(configuration);
+var jsonRpcClient = null;
 
 
 let server = jayson.server({
 
+
+    getKurentoClient : function(params,callback) {
+
+        var clusterId = params[0];
+        var configuration = {
+            sendCloseMessage : false,
+            ws : {
+              uri : ws_uri[clusterId],
+              useSockJS: false,
+              onconnected : connectCallback,
+              ondisconnect : disconnectCallback,
+              onreconnecting : disconnectCallback,
+              onreconnected : connectCallback,
+              onerror : errorCallback
+            },
+            rpc : {
+              requestTimeout : 15000,
+              onEvent : onEvent
+            }
+        };
+        
+        jsonRpcClient = new JsonRpcClient(configuration);
+        callback(null,'clustering id= ' +clusterId);   
+    },
     createPipeline : function(params,callback){ 
         jsonRpcClient.send('create',params,function(err, _pipeline) {
             if(err) return callback(err);
@@ -113,4 +139,5 @@ let server = jayson.server({
     }
 });
 
+// server.http().listen(8088,process.env.HOST);
 server.http().listen(8088,process.env.HOST);
